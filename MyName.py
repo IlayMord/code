5dd2f1b52b24d6ba5290cfb930a1c1da2275d6d6
Ilay Mor <ilaym1000@gmail.com>
2025-09-22 14:16:08 +0300

    new file

--- MyName.py
+++ MyName.py
@@ -1,7 +1,8 @@
-
 name = input(str("pls enter your name:"))
 def hello(name):
     if name == "ilay": 
-        king = name + " the king"
+        king = name + " the king!"
         print(f"hello {king}")
+    elif name != "ilay":
+        print("you " + name + " are not ilay")
 hello(name)
